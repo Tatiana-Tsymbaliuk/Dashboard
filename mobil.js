b3389350afe84f021ea7cfd61943602c824455e7
TetianaTS <devse.ten@uwp.digital>
2024-08-16 23:45:10 +0200

    create finish

--- mobil.js
+++ mobil.js
@@ -1,3 +1,12 @@
-document.querySelector(".header__menu-btn").addEventListener("click", () => {
-  document.querySelector(".sidebar").classList.toggle("sidebar--visible");
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtn = document.querySelector(".menu-btn");
+  const sidebar = document.querySelector(".sidebar");
+
+  if (menuBtn && sidebar) {
+    menuBtn.addEventListener("click", () => {
+      sidebar.classList.toggle("sidebar--visible");
+    });
+  } else {
+    console.error("Не удалось найти элементы для меню или сайдбара.");
+  }
 });
--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const filterBtn = document.querySelector(".customers__btn");
-  const rows = document.querySelectorAll(".table__row");
+  const filterBtn = document.querySelector(".customers__header-active");
+  const rows = document.querySelectorAll(".customers__table-row");
 
-  filterBtn.addEventListener("click", () => {
-    rows.forEach((row) => {
-      const statusCell = row.querySelector(".table__cell--status");
-      if (statusCell && statusCell.textContent !== "Active") {
-        row.style.display = "none";
-      } else {
-        row.style.display = "";
-      }
+  if (filterBtn) {
+    filterBtn.addEventListener("click", () => {
+      rows.forEach((row) => {
+        const statusCell = row.querySelector(
+          ".customers__table-row td:last-child"
+        );
+        if (statusCell && statusCell.textContent.trim() !== "Active") {
+          row.style.display = "none";
+        } else {
+          row.style.display = "";
+        }
+      });
     });
-  });
+  } else {
+    console.error("Элемент с классом 'customers__header-active' не найден.");
+  }
 });
